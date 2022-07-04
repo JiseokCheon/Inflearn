f424df4cd4bde39107403ecb708bfa7b30d9c9ce
jscheon <cjswltjr159@naver.com>
2022-07-04 21:12:20 +0900

    add : Kafka  OrderSerivce

--- spring_cloud/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ spring_cloud/order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.orderservice.controller;
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
 import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -16,21 +17,24 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/order-service")
 public class OrderController {
-
     Environment env;
     OrderService orderService;
     KafkaProducer kafkaProducer;   // 등록한 빈 사용
+    OrderProducer orderProducer;
 
     @Autowired
-    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer) {
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer, OrderProducer orderProducer) {
         this.env = env;
         this.orderService = orderService;
         this.kafkaProducer = kafkaProducer;
+        this.orderProducer = orderProducer;
     }
+
 
     @GetMapping("/health_check")
     public String status() {
@@ -43,15 +47,24 @@ public class OrderController {
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
+
+
         orderDto.setUserId(userId);
-        orderService.createOrder(orderDto);
+
+        // JPA
+//        orderService.createOrder(orderDto);
+//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+
+        // Kafka
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
 
         // kafka 에 주문 전달
         kafkaProducer.send("example-catalog-topic", orderDto);
+        orderProducer.send("orders", orderDto);
 
 
         ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
-
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
 
