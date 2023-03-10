28d786b6ce7a4399d14381f486935f24378c9199
천지석 <cjswltjr159@naver.com>
2023-03-10 22:45:43 +0900

    Hello Spring Batch 시작하기

---
+++ spring-batch/src/main/java/com/example/springbatch/config/HelloJobConfiguration.java
@@ -0,0 +1,70 @@
+package com.example.springbatch.config;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.builder.SimpleJobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class HelloJobConfiguration {
+
+	/*
+	Spring Batch 5 부터 사용변경
+
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job helloJob() {
+		return jobBuilderFactory.get("helloJob").start(helloStep()).build();
+	}
+
+	@Bean
+	public Step helloStep() {
+		return stepBuilderFactory.get("helloStep")
+			.tasklet(((contribution, chunkContext) -> {
+				System.out.println("Hello Spring Batch");
+				return RepeatStatus.FINISHED;
+			})).build();
+	}
+	 */
+
+
+	/*
+		사용자가 문서를 읽어 인지하지 않는 한 Builder에서 JobRepository가 생성되고 설정된다는 사실을 숨기고 있다.
+		그래서 아래와 같이 JobRepository과 PlatformTransactionManager를 명시적으로 제공하는 방식을 사용하길 권장한다.
+	 */
+
+	@Bean
+	public Job helloJob(JobRepository jobRepository, Step step) {
+		return new JobBuilder("helloJob", jobRepository)
+			.incrementer(new RunIdIncrementer())
+			.flow(step)
+			.end()
+			.build();
+	}
+
+	@Bean
+	public Step helloStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return new StepBuilder("helloStep", jobRepository)
+			.tasklet((contribution, chunkContext) -> {
+				System.out.println("Hello Spring Batch");
+				return RepeatStatus.FINISHED;
+			}, transactionManager)
+			.build();
+	}
+
+}
